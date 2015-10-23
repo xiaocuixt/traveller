9d7df96f67989a7e0e0ed62b3231f2031b8b6df3
xiaocuixt <cjw624923@126.com>
2015-10-23 14:15:52 +0800

    用户详情页

---
+++ traveller/FindViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FindViewController.swift
+//  traveller
+//
+//  Created by xiaocui on 15/10/23.
+//  Copyright (c) 2015年 xiaocui. All rights reserved.
+//
+
+import UIKit
+
+class FindViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var findScroll: UIScrollView!
+    
+    @IBOutlet weak var findPageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var timer:NSTimer
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- traveller/WelcomeViewController.swift
+++ traveller/WelcomeViewController.swift
@@ -64,9 +64,9 @@ class WelcomeViewController: UIViewController, UIScrollViewDelegate {
     }
     func buttonClick(button:UIButton)
     {
-        //let sb = UIStoryboard(name: "Main", bundle: nil)
-        //let mainView = sb.instantiateViewControllerWithIdentifier("") as! UIViewController
-        //self.presentViewController(mainView, animated: true, completion: nil)
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let list = sb.instantiateViewControllerWithIdentifier("list") as! UIViewController
+        self.presentViewController(list, animated: true, completion: nil)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
